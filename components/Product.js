07e8bfaee4210de40e9db617490f726afabb0bb9
Thu Nguyen <tmnguyen@berkeley.edu>
2020-02-25 00:51:14 -0800

    added productimages, fixed store lookup in clerk login

--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Image, View } from 'react-native';
+import React from "react";
+import { Image, View } from "react-native";
 
-import { ProductBody, ProductContainer } from '../styles.js';
+import { ProductBody, ProductContainer } from "../styles.js";
 
 /**
  * @prop
@@ -10,9 +10,11 @@ import { ProductBody, ProductContainer } from '../styles.js';
 // TODO @tommypoa to use styled-components
 function Product({ product }) {
   return (
-    <View style={{ alignItems: 'center' }}>
+    <View style={{ alignItems: "center" }}>
       <Image
-        source={require('../assets/images/robot-dev.png')}
+        source={{
+          uri: product.image
+        }}
         style={{ width: 50, height: 50, borderRadius: 80 / 2 }}
       />
       <ProductBody>{product.name}</ProductBody>
