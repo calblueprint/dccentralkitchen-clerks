ab4314439ddae3017bb75a7ac630570677dd0d10
annie.ro98 <anniero@berkeley.edu>
2020-02-26 17:18:09 -0800

    Development lifecycle updates

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react-native', 'prettier'],
+  rules: {
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'no-use-before-define': ['error', { variables: false }],
+    'no-const-assign': 'warn',
+    'no-this-before-super': 'warn',
+    'no-undef': 'warn',
+    'no-unreachable': 'warn',
+    'no-unused-vars': 'warn',
+    'constructor-super': 'warn',
+    'valid-typeof': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'prettier/prettier': ['error'],
+    'react/destructuring-assignment': ['enabled', 'never']
+  }
+};
