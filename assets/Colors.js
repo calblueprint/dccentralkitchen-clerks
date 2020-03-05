faa6b8fdfbb62b15d9510f0adce4828eb7d3d796
annie.ro98 <anniero@berkeley.edu>
2020-03-04 17:25:09 -0800

    Update colors

--- assets/Colors.js
+++ assets/Colors.js
@@ -4,11 +4,6 @@ const Colors = {
   lighterGreen: '#48AC69',
   primaryGreen: '#008550',
   darkerGreen: '#004E14',
-  // Orange
-  lightestOrange: '#FFD9B1',
-  lighterOrange: '#F7AA68',
-  primaryOrange: '#F07723',
-  darkerOrange: '#8F3000',
   // Text
   activeText: 'rgba(0, 0, 0, 0.8)',
   secondaryText: '#999999',
