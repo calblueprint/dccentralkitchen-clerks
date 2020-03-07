4dfa7fb6cde983f65e55bfb4f2464a286c84e0ff
Thu Nguyen <tmnguyen@berkeley.edu>
2020-03-06 16:15:03 -0800

    simplified ternary operators in base components

--- components/BaseComponents.js
+++ components/BaseComponents.js
@@ -6,28 +6,28 @@ export const Title = styled.Text`
   font-family: poppins-medium;
   font-size: 20px;
   line-height: 30px;
-  color: ${props => (props.color ? props.color : Colors.black)};
+  color: ${props => props.color || Colors.black};
 `;
 
 export const Subhead = styled.Text`
   font-family: poppins-regular;
   font-size: 16px;
   line-height: 24px;
-  color: ${props => (props.color ? props.color : Colors.black)};
+  color: ${props => props.color || Colors.black};
 `;
 
 export const Body = styled.Text`
   font-family: poppins-regular;
   font-size: 14px;
   line-height: 20px;
-  color: ${props => (props.color ? props.color : Colors.black)};
+  color: ${props => props.color || Colors.black};
 `;
 
 export const Caption = styled.Text`
   font-family: poppins-medium;
   font-size: 12px;
   line-height: 16px;
-  color: ${props => (props.color ? props.color : Colors.black)};
+  color: ${props => props.color || Colors.black};
 `;
 
 export const Overline = styled.Text`
@@ -35,14 +35,7 @@ export const Overline = styled.Text`
   font-size: 14px;
   line-height: 20px;
   text-transform: uppercase;
-  color: ${props => (props.color ? props.color : Colors.black)};
-`;
-
-export const TabSelected = styled.Text`
-  font-family: poppins-semibold;
-  font-size: 14px;
-  line-height: 20px;
-  color: ${props => (props.color ? props.color : Colors.black)};
+  color: ${props => props.color || Colors.black};
 `;
 
 // Buttons
@@ -63,7 +56,7 @@ export const ButtonLabel = styled.Text`
 `;
 
 export const FilledButtonContainer = styled(ButtonContainer)`
-  width: 309px;
-  height: 57px;
+  width: ${props => props.width || '309px'};
+  height: ${props => props.height || '57px'}
   background: ${props => (props.color ? props.color : Colors.primaryGreen)};
 `;
