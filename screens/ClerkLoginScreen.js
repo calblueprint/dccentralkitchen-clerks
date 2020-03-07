16ef67a1348ebb69076c8fadb22cd6b861cd5976
annie.ro98 <anniero@berkeley.edu>
2020-03-06 18:48:05 -0800

    Add error handling for clerk login and customer lookup

--- screens/ClerkLoginScreen.js
+++ screens/ClerkLoginScreen.js
@@ -1,99 +1,116 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import {
-  AsyncStorage,
-  Keyboard,
-  Picker,
-  TouchableWithoutFeedback
-} from 'react-native';
+import { AsyncStorage, Button, Keyboard, Picker, Text, TouchableWithoutFeedback } from 'react-native';
+import { status } from '../lib/constants';
 import { loadStoreData, lookupClerk } from '../lib/loginUtils';
-import { Container, SubmitButton, TextInput } from '../styled/shared.js';
+import { Container, SubmitButton, TextInput } from '../styled/shared';
 
-//TODO rename this
+// TODO rename this
 const DismissKeyboard = ({ children }) => (
-  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-    {children}
-  </TouchableWithoutFeedback>
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>{children}</TouchableWithoutFeedback>
 );
 
 export default class ClerkLoginScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      storeName: '',
+      stores: [],
       storeId: '',
       password: '',
-      stores: [],
-      displayErrorMsg: '' // TODO @tommypoa: isLoading
+      errorMsg: null
     };
   }
 
   async componentDidMount() {
-    const stores = await loadStoreData();
-    // Set to first store as default, since the picker also defaults to the top (first in list)
-    this.setState({
-      stores: stores,
-      storeName: stores[0].name,
-      storeId: stores[0].id
-    });
+    try {
+      const stores = await loadStoreData();
+      // Set to first store as default, since the picker also defaults to the top (first in list)
+      this.setState({
+        stores,
+        storeId: stores[0].id
+      });
+    } catch (err) {
+      console.error(err);
+    }
   }
+
+  // Purely to bypass the flow for development -- go straight to Checkout.
+  // Configures to use Jeffry Poa & Robin Hood
+  _devBypass = async () => {
+    await AsyncStorage.setItem('clerkId', 'recgq59j7Cx9zsSYE');
+    await AsyncStorage.setItem('storeId', 'recw49LpAOInqvX3e');
+    await AsyncStorage.setItem('customerId', 'recqx32YmmACiRWMq');
+    this.props.navigation.navigate('Checkout');
+  };
 
   // Set the clerkId and storeId in AsyncStorage
   // Then navigate to the customer lookup screen
-  _asyncLoginClerk = async recordId => {
-    await AsyncStorage.setItem('clerkId', recordId);
+  _asyncLoginClerk = async clerkRecord => {
+    await AsyncStorage.setItem('clerkId', clerkRecord.id);
     await AsyncStorage.setItem('storeId', this.state.storeId);
-    this.props.navigation.navigate('CustomerLookup');
+    this.props.navigation.navigate('CustomerLookup', { clerkName: clerkRecord.clerkName });
   };
 
   // This function will sign the user in if the clerk is found.
   async handleSubmit() {
-    // Uses the `Store ID` lookup in AirTable
-    await lookupClerk(this.state.storeId, this.state.password)
-      .then(resp => {
-        if (resp) {
-          const recordId = resp;
-          this._asyncLoginClerk(recordId);
-        }
-      })
-      .catch(err => {
-        console.error('Error submitting form', err);
-      });
+    try {
+      // Uses the `Store ID` lookup in AirTable
+      const lookupResult = await lookupClerk(this.state.storeId, this.state.password);
+
+      let clerkRecord = null;
+      switch (lookupResult.status) {
+        case status.MATCH:
+          clerkRecord = lookupResult.record;
+          this._asyncLoginClerk(clerkRecord);
+          break;
+        // TODO for production, we should have some sort of logging mechanism (i.e replacing console logs)
+        case status.FOUND:
+          console.log('Incorrect password');
+          break;
+        case status.NOT_FOUND:
+          console.log('No clerk found at this store');
+          break;
+        case status.DUPLICATE:
+          console.log('Database malformed! Two users found');
+          break;
+        default:
+          return;
+      }
+      this.setState({ errorMsg: lookupResult.errorMsg, password: '' });
+    } catch (err) {
+      console.error('Airtable: ', err);
+    }
   }
 
   render() {
     return (
-      // TODO break out this onChange into a function
       <DismissKeyboard>
         <Container>
           <Picker
             mode="dropdown"
-            onValueChange={store => this.setState({ storeId: store })}
+            onValueChange={storeId => this.setState({ storeId })}
             selectedValue={this.state.storeId}>
-            {this.state.stores.map((item, index) => {
-              return (
-                <Picker.Item
-                  label={item['name']}
-                  value={item['id']}
-                  key={index}
-                />
-              );
+            {this.state.stores.map(store => {
+              return <Picker.Item label={store.storeName} value={store.id} key={store.id} />;
             })}
           </Picker>
           <TextInput
             placeholder="Password"
             keyboardType="number-pad"
             maxLength={4}
-            secureTextEntry={true}
+            secureTextEntry
             onChangeText={text => this.setState({ password: text })}
             value={this.state.password}
           />
-          <SubmitButton
-            color="#008550"
-            title="Log In"
-            onPress={() => this.handleSubmit()}
-          />
+          <SubmitButton color="#008550" title="Log In" onPress={() => this.handleSubmit()} />
+          {this.state.errorMsg ? <Text>{this.state.errorMsg}</Text> : null}
+          <Button title="Testing Bypass" onPress={() => this._devBypass()} />
         </Container>
       </DismissKeyboard>
     );
   }
 }
+
+ClerkLoginScreen.propTypes = {
+  navigation: PropTypes.object.isRequired
+};
