41db4018f74d6835a30b1ba330515918af23ff96
Kenneth Lien <kennethlien@berkeley.edu>
2020-03-23 13:11:21 -0700

    fixing header and link typo

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -69,7 +69,7 @@ export class DrawerContent extends React.Component {
           {console.log(this.state.clerkName)}
           <Title style={{ color: 'white' }}>{this.state.clerkName}</Title>
         </View>
-        <TouchableOpacity style={{ padding: 16 }} onPress={() => Linking.openURL('tiny.cc/SubmitFeedbackClerk')}>
+        <TouchableOpacity style={{ padding: 16 }} onPress={() => Linking.openURL('http://tiny.cc/SubmitFeedbackClerk')}>
           <Title>Feedback</Title>
         </TouchableOpacity>
         <TouchableOpacity style={{ paddingLeft: 16 }} onPress={() => Linking.openURL('http://tiny.cc/ClerkFeedback')}>
@@ -117,6 +117,7 @@ export default createAppContainer(
         screen: MyDrawerNavigator
       }
     },
+    { headerMode: 'none' },
     {
       initialRouteName: 'Auth'
     }
