49b072abe65877adb1d425bcb81bfed855096da6
annie.ro98 <anniero@berkeley.edu>
2020-04-19 16:28:47 -0700

    Update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,17 +13,43 @@ module.exports = {
   },
   plugins: ['react-native', 'prettier'],
   rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'airtable',
+            message: 'Do not use the airtable module outside of airtable.js',
+          },
+          {
+            name: './airtable',
+            message: 'Do not use airtable.js outside of request.js',
+          },
+          {
+            name: '../../lib/airtable',
+            message: 'Do not use airtable.js outside of request.js',
+          },
+          {
+            name: '../../../lib/airtable',
+            message: 'Do not use airtable.js outside of request.js',
+          },
+        ],
+      },
+    ],
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
     'no-use-before-define': ['error', { variables: false }],
     'no-const-assign': 'warn',
     'no-this-before-super': 'warn',
     'no-undef': 'warn',
     'no-unreachable': 'warn',
-    'no-unused-vars': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'constructor-super': 'warn',
     'valid-typeof': 'warn',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'prettier/prettier': ['error'],
-    'react/destructuring-assignment': 'warn',
+    'react/destructuring-assignment': 'off',
+    'react/forbid-prop-types': ['on', { forbid: ['any'] }],
+    'react/jsx-closing-bracket-location': 'off',
+    'react/jsx-wrap-multilines': 'off',
   },
 };
