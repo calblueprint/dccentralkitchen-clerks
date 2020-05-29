37513e46462c87b6f960c4020dd1b8071c66b9d8
Annie Wang <anniedwang02@gmail.com>
2020-05-29 18:25:09 -0400

    Remove unused assets

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet } from 'react-native';
 import * as Sentry from 'sentry-expo';
+import Colors from './constants/Colors';
 import { env } from './environment';
 import AppNavigator from './navigation/AppNavigator';
 import { Container } from './styled/shared';
@@ -13,7 +13,6 @@ import { Container } from './styled/shared';
 Sentry.init({
   dsn: 'https://9b39ef5b02394775a6e939254ed55a0e@o306199.ingest.sentry.io/4525076',
   enableInExpoDevelopment: false,
-  release: 'v1.1.1',
   debug: true,
   environment: env,
 });
@@ -41,13 +40,10 @@ export default function App(props) {
 
 async function loadResourcesAsync() {
   await Promise.all([
-    Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
     Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      // This is the icon style we use
+      ...FontAwesome5.font,
+      // Poppins is the custom font used across the application
       'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
       'poppins-semibold': require('./assets/fonts/Poppins-SemiBold.ttf'),
       'poppins-medium': require('./assets/fonts/Poppins-Medium.ttf'),
@@ -68,6 +64,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.bgLight,
   },
 });
